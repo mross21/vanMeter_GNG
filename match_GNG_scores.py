59c75b8022b7492a073786d3b47ff3df1b01460d
mross21 <89810375+mross21@users.noreply.github.com>
2024-01-04 19:51:22 -0600

    update

--- match_GNG_scores.py
+++ match_GNG_scores.py
@@ -6,14 +6,15 @@ from collections import defaultdict
 
 gngFile = '/home/mindy/Desktop/BiAffect-iOS/vanMeter/processed_output/AllUsers_GNGdata_fromZIP_allResponseTimes.csv'
 IDlinkFile = '/home/mindy/Desktop/BiAffect-iOS/vanMeter/raw_input/ID_link.csv'
-# participantFile = '/home/mindy/Desktop/BiAffect-iOS/vanMeter/raw_input/participants.csv'
 smsFile = '/home/mindy/Desktop/BiAffect-iOS/vanMeter/raw_input/BiAffect_SMS.csv'
+interviewFile = '/home/mindy/Desktop/BiAffect-iOS/vanMeter/raw_input/BiAffect_Interview_Data_longFormat.csv'
+selfReportFile = '/home/mindy/Desktop/BiAffect-iOS/vanMeter/raw_input/BiAffect_Participant_Self-Report.csv'
 
 dfGNG = pd.read_csv(gngFile, index_col=False)
 dfIDs = pd.read_csv(IDlinkFile, index_col=False)
-# dfParticipants = pd.read_csv(participantFile, index_col=False)
 dfSMS = pd.read_csv(smsFile, index_col=False)
-
+dfInterview = pd.read_csv(interviewFile, index_col=False)
+dfSelfReport = pd.read_csv(selfReportFile, index_col=False)
 
 # match userIDs to IDs
 dictIDs = dict(zip(dfIDs['Health Code'], dfIDs['ID']))
@@ -28,61 +29,110 @@ dfSMS['date'] = np.where(dfSMS['date'] == '1/0/00 0:00', float('NaN'), dfSMS['da
 dfSMS.dropna(subset=['date'], inplace=True)
 dfSMS['date'] = pd.to_datetime(dfSMS['date'].str.split(' ').str[0], format = '%m/%d/%y').dt.date
 
+# set pd.datetime
 dfGNG['date'] = pd.to_datetime(dfGNG['date']).dt.date
+dfInterview['weekDate'] = pd.to_datetime(dfInterview['weekDate']).dt.date
+dfInterview['weekDateEnd'] = pd.to_datetime(dfInterview['weekDateEnd']).dt.date
+dfSelfReport['date'] = pd.to_datetime(dfSelfReport['date']).dt.date
 
 
 outGNG = []
-dictSleep = defaultdict(dict)
-dictSI = defaultdict(dict)
-dictDesire = defaultdict(dict)
-dictIntent = defaultdict(dict)
-dictSH = defaultdict(dict)
+# dictSleep = defaultdict(dict)
+# dictSI = defaultdict(dict)
+# dictDesire = defaultdict(dict)
+# dictIntent = defaultdict(dict)
+# dictSH = defaultdict(dict)
 
 for ID in IDlist:
     grpSMS = dfSMS.loc[dfSMS['ID'] == ID]
     grpGNG = dfGNG.loc[dfGNG['studyID'] == ID]
+    grpInterview = dfInterview.loc[dfInterview['participant_id'] == ID]
+    grpSelfReport = dfSelfReport.loc[dfSelfReport['participant_id'] == ID]
 
+    # add month start date to self-report file
+    grpSelfReport['monthStart'] = np.where((grpSelfReport['date'] - pd.Timedelta(30, unit='days')) < grpSelfReport['date'].shift(1),
+                                        grpSelfReport['date'].shift(1), (grpSelfReport['date'] - pd.Timedelta(30, unit='days')))
+    
+    # get age, sex, and gender
+    age = np.unique(grpSelfReport['age_baseline_pt'])
+    age = age[~np.isnan(age)][0]
+    sex = np.unique(grpSelfReport['baseline_sex'])
+    sex = sex[~np.isnan(sex)][0]
+    gender = np.unique(grpSelfReport['baseline_gender'])
+    gender = gender[~np.isnan(gender)][0]
+
+    
     dateList = grpGNG['date'].unique()
 
     for date in dateList:
         # print(f'GNG date: {date}')
+        gngTask = grpGNG.loc[grpGNG['date'] == date]
+
+        gngTask['age'] = age
+        gngTask['sex'] = sex
+        gngTask['gender'] = gender
+
+        # SMS
         grpSMS['diffDays'] = grpSMS['date'] - date
         matchDate = grpSMS.loc[grpSMS['diffDays'] == grpSMS['diffDays'].min()]
         if len(matchDate) < 1:
-            sleep = np.nan
-            SI = np.nan
-            desire = np.nan
-            intent = np.nan
-            sh = np.nan
+            gngTask['sms_sleep'] = np.nan
+            gngTask['sms_SI'] = np.nan
+            gngTask['sms_desire'] = np.nan
+            gngTask['sms_intent'] = np.nan
+            gngTask['sms_self_harm'] = np.nan
         elif grpSMS['diffDays'].min() <= pd.Timedelta(7,unit='days'):
-            sleep = matchDate['sleep'].mean()
-            SI = matchDate['si_current_y_n'].mean()
-            desire = matchDate['desire_to_die'].mean()
-            intent = matchDate['suicide_intent'].mean()
-            sh = matchDate['any_self_harm_y_n'].mean()
+            gngTask['sms_sleep'] = matchDate['sleep'].mean()
+            gngTask['sms_SI'] = matchDate['si_current_y_n'].mean()
+            gngTask['sms_desire'] = matchDate['desire_to_die'].mean()
+            gngTask['sms_intent'] = matchDate['suicide_intent'].mean()
+            gngTask['sms_self_harm'] = matchDate['any_self_harm_y_n'].mean()
         else:
-            sleep = np.nan
-            SI = np.nan
-            desire = np.nan
-            intent = np.nan
-            sh = np.nan
+            gngTask['sms_sleep'] = np.nan
+            gngTask['sms_SI'] = np.nan
+            gngTask['sms_desire'] = np.nan
+            gngTask['sms_intent'] = np.nan
+            gngTask['sms_self_harm'] = np.nan
+
+        # INTERVIEW
+        interviewRow = grpInterview.loc[(grpInterview['weekDate'] <= date) & 
+                                         (grpInterview['weekDateEnd'] >= date)]
+        
+        gngTask['interview_dep_score'] = interviewRow['dep_score'].mean()
+        gngTask['interview_SI_score'] = interviewRow['suic_score'].mean()
+        gngTask['interview_mania_score'] = interviewRow['mania_score'].mean()
+        gngTask['interview_anx_score'] = interviewRow['anx_score'].mean()
 
 
 
-        dictSleep[ID][date] = sleep
-        dictSI[ID][date] = SI
-        dictDesire[ID][date] = desire
-        dictIntent[ID][date] = intent
-        dictSH[ID][date]= sh
-  
-dfGNG['sleep'] = dfGNG.apply(lambda x: dictSleep[x['studyID']][x['date']], axis=1)
-dfGNG['SI'] = dfGNG.apply(lambda x: dictSI[x['studyID']][x['date']], axis=1)
-dfGNG['desire'] = dfGNG.apply(lambda x: dictDesire[x['studyID']][x['date']], axis=1)
-dfGNG['intent'] = dfGNG.apply(lambda x: dictIntent[x['studyID']][x['date']], axis=1)
-dfGNG['self_harm'] = dfGNG.apply(lambda x: dictSH[x['studyID']][x['date']], axis=1)
+        # self-report
+        selfReportRow = grpSelfReport.loc[(grpSelfReport['date'] >= date) & (grpSelfReport['monthStart'] < date)]
+
+        gngTask['self_report_dep_score'] = selfReportRow['self_report_depression'].mean()
+        gngTask['self_report_mania_score'] = selfReportRow['self_report_mania'].mean()
+        gngTask['self_report_sleepDisturbance_score'] = selfReportRow['sleep_disturbance'].mean()
+        gngTask['self_report_anx_score'] = selfReportRow['sleep_disturbance'].mean()
+
+        # append GNG
+        outGNG.append(gngTask)
+
+dfOut = pd.concat(outGNG)
+
+        # # dict of ID, date, and scores
+        # dictSleep[ID][date] = sleep
+        # dictSI[ID][date] = SI
+        # dictDesire[ID][date] = desire
+        # dictIntent[ID][date] = intent
+        # dictSH[ID][date]= sh
+
+# dfGNG['sleep'] = dfGNG.apply(lambda x: dictSleep[x['studyID']][x['date']], axis=1)
+# dfGNG['SI'] = dfGNG.apply(lambda x: dictSI[x['studyID']][x['date']], axis=1)
+# dfGNG['desire'] = dfGNG.apply(lambda x: dictDesire[x['studyID']][x['date']], axis=1)
+# dfGNG['intent'] = dfGNG.apply(lambda x: dictIntent[x['studyID']][x['date']], axis=1)
+# dfGNG['self_harm'] = dfGNG.apply(lambda x: dictSH[x['studyID']][x['date']], axis=1)
 
 
-dfGNG.to_csv('/home/mindy/Desktop/BiAffect-iOS/vanMeter/gng/gng_sms_processed_output.csv', index=False)
+dfOut.to_csv('/home/mindy/Desktop/BiAffect-iOS/vanMeter/gng/gng_sms_processed_output-v2.csv', index=False)
 
 #%%
 # # MATCH SMS TO EXACT GNG DATE
---
+++ reformat_biaffectInterviewFile.py
@@ -0,0 +1,37 @@
+#%%
+import pandas as pd
+
+file = '/home/mindy/Desktop/BiAffect-iOS/vanMeter/raw_input/BiAffect_Interview_Data.csv'
+df = pd.read_csv(file, index_col=False)
+
+df['psr_startdate'] = pd.to_datetime(df['psr_startdate']).dt.date
+df['psr_enddate'] = pd.to_datetime(df['psr_enddate']).dt.date
+
+dfDep = df.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,11,12]]
+dfSuic = df.iloc[:,[0,1,2,3,13,14,15,16,17,18,19,20,21]]
+dfMania = df.iloc[:,[0,1,2,3,22,23,24,25,26,27,28,29,30]]
+dfAnx = df.iloc[:,[0,1,2,3,31,32,33,34,35,36,37,38,39]]
+
+dfList = [dfDep, dfSuic, dfMania, dfAnx]
+dfOut = pd.melt(dfDep, id_vars=['participant_id','psr_startdate','psr_enddate','prs_numweeks'],
+            var_name='weekFromEnd', value_name='score').sort_values(by=['participant_id',
+            'psr_startdate']).dropna(subset = ['score']).reset_index(drop=True)
+dfOut['weekFromEnd'] = dfOut['weekFromEnd'].str.extract('(\d+)').astype(int)
+dfOut = dfOut.iloc[:,0:5]
+
+for d in dfList:
+    dfLong = pd.melt(d, id_vars=['participant_id','psr_startdate','psr_enddate','prs_numweeks'],
+            var_name='weekFromEnd', value_name=('score')).sort_values(by=['participant_id','psr_startdate'])
+    dfLong = dfLong.dropna(subset = ['score']).reset_index(drop=True)
+    score_type = dfLong['weekFromEnd'].str.split('_').str[0][0]
+    dfLong.rename(columns={"score": score_type + '_score'}, inplace=True)
+    dfLong['weekFromEnd'] = dfLong['weekFromEnd'].str.extract('(\d+)').astype(int)
+    dfLong.insert(5, 'weekDate', dfLong['psr_enddate'] - (dfLong['weekFromEnd'] * pd.Timedelta(7, 'days')) - pd.Timedelta(7, 'days'))
+    dfLong.insert(6, 'weekDateEnd', dfLong['psr_enddate'] - (dfLong['weekFromEnd'] * pd.Timedelta(7, 'days')))
+
+    dfOut = pd.merge(dfOut, dfLong, how='outer')
+    
+
+dfOut.to_csv('/home/mindy/Desktop/BiAffect-iOS/vanMeter/raw_input/BiAffect_Interview_Data_longFormat.csv', index=False)
+
+# %%
